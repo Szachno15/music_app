deac5f11af74db80fb7c2986d61ae57be093241d
Krystian Szachno <krystianszachno@Krystians-MacBook-Pro.local>
2018-07-29 13:39:53 +0100

    Added details page

--- music_apps/urls.py
+++ music_apps/urls.py
@@ -4,4 +4,5 @@ from django.contrib import admin
 from . import views
 urlpatterns = [
     url(r'^index/$', views.index, name='index'),
+    url(r'^detail/$', views.detail, name='detail'),
 ]
--- music_apps/views.py
+++ music_apps/views.py
@@ -2,6 +2,12 @@
 from __future__ import unicode_literals
 from django.http import HttpResponse
 from django.shortcuts import render
+from .models import Album, Song
 
 def index(request):
 	return render(request, 'music_apps/index.html') 
+	
+def detail(request):
+	album = Album.objects.all()
+	context = {'album':album}
+	return render(request, 'music_apps/detail.html', context)
