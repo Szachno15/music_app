318ca9b39af6e0c1e8854905477f00b876a4d83d
Krystian <szachnok@gmail.com>
2018-08-15 19:15:42 +0100

    rm pyc files

--- music_app/settings.py
+++ music_app/settings.py
@@ -128,21 +128,17 @@ BOOTSTRAP3 = {
 	'include_jquery':True,
 	}
 	# Heroku settings
-	cwd = os.getcwd()
-	if cwd == '/app' or cwd[:4] == '/tmp':
-		import dj_database_url
-		DATABASES = {
-			'default': dj_database_url.config(default='postgres://localhost')
-		}
+cwd = os.getcwd()
+if cwd == '/app' or cwd[:4] == '/tmp':
+	import dj_database_url
+	DATABASES = {'default': dj_database_url.config(default='postgres://localhost')}
 
 	#Honor the 'X-Forwarded-Proto' header for request.is_secure().
-	SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'http')
 	
 	# Allow all host headers
-	ALLOWED_HOST = ['*']
+ALLOWED_HOST = ['*']
 	
 	#Static asset configuration
-	BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-	STATICFILES_DIRS = (
-		os.path.join(BASE_DIR, 'static'),
-	)
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'))
